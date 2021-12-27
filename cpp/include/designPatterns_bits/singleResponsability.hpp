1277f8e872e90605395fddf5bfa906d7acc97cb9
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2021-12-27 23:32:11 +0100

    training with the single responsability principle

--- cpp/include/designPatterns_bits/singleResponsability.hpp
+++ cpp/include/designPatterns_bits/singleResponsability.hpp
@@ -10,25 +10,25 @@ struct Journal
 {
     string title;
     vector<string> entries;
+
     explicit Journal(const string &title) : title{title}
     {
     }
 
     void add(const string &entry);
 
-    // single concern with add
     void save(const string &filename);
 };
 
 void Journal::add(const string &entry)
 {
     static int count = 1;
-
-    entries.push_back(boost::lexical_cast<string>(count++) + ": " + entry);
+    entries.push_back(boost::lexical_cast<string>(count++) + " : " + entry);
 }
 
 void Journal::save(const string &filename)
 {
+
     ofstream ofs(filename);
     for (auto &s : entries)
     {
@@ -36,12 +36,10 @@ void Journal::save(const string &filename)
     }
 }
 
-// Too many persistences -> we create a Manager for saving for journals and open for others classes
-
 struct PersistenceManager
 {
 
-    static void save(const Journal &j, const string &filename)
+    void save(const Journal &j, const string &filename)
     {
         ofstream ofs(filename);
         for (auto &s : j.entries)
@@ -50,3 +48,10 @@ struct PersistenceManager
         }
     }
 };
+
+/*
+The methode is the "work" of the class.
+The save is too much in violates the single responsability concept.
+
+Too many persistences -> we create a Manager for saving for journals and open for others classes
+*/
