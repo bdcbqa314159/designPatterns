9e355a97aa8a891e81417a26deb9d202e825f7e7
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-05 10:10:22 +0100

    Lyskov Substitution Principle totally understood.

--- cpp/include/designPatterns_bits/lyskovSubstitution.hpp
+++ cpp/include/designPatterns_bits/lyskovSubstitution.hpp
@@ -5,23 +5,13 @@
 
 class Rectangle
 {
+
 protected:
-    int width;
-    int height;
+    int width, height;
 
 public:
     Rectangle(int width, int height) : width(width), height(height)
     {
-    }
-
-    virtual void set_height(const int height)
-    {
-        this->height = height;
-    }
-
-    virtual void set_width(const int width)
-    {
-        this->width = width;
     }
 
     int get_height() const
@@ -33,6 +23,17 @@ public:
     {
         return width;
     }
+
+    virtual void set_width(int width)
+    {
+        this->width = width;
+    }
+
+    virtual void set_height(int height)
+    {
+        this->height = height;
+    }
+
     int area() const
     {
         return height * width;
@@ -47,27 +48,25 @@ public:
     {
     }
 
-    void set_height(int height)
-    {
-        this->height = this->width = height;
-    }
-
     void set_width(int width)
     {
         this->width = this->height = width;
+    }
+
+    void set_height(int height)
+    {
+        this->height = this->width = height;
     }
 };
 
 void process(Rectangle &r)
 {
-
     int w = r.get_width();
     r.set_height(10);
 
-    std::cout << "Expected area : " << w * 10 << std::endl;
-    std::cout << "Actual area : " << r.area() << std::endl;
+    std::cout << "Expected area " << 10 * w << std::endl;
+    std::cout << "Actual area " << r.area() << std::endl;
 }
-
 // Objects in a program should be replaceable with instances of their subtypes without altering the correctness of the program.
 // The design above does not take care if Rectangle has subclasses like Square, the design is broken and Lyskov principle is violated.
 // If we want this process API keep going we need to set Square out of inheritance because process is not suitable for square anyway.
