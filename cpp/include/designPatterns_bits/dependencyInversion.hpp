d225ae076c689664e8dfbb3aa6db1b1be5294598
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-07 03:21:52 +0000

    keep training with dependency inversion principle.

--- cpp/include/designPatterns_bits/dependencyInversion.hpp
+++ cpp/include/designPatterns_bits/dependencyInversion.hpp
@@ -10,19 +10,23 @@ using namespace std;
 
 enum class Relationship
 {
+
     parent,
     child,
-    sibbling
+    sibling
 };
 
 struct Person
 {
+
     string name;
-    Person(string name) : name(name) {}
+
+    Person(string name) : name(name){};
 };
 
 struct RelationshipBrowser
 {
+
     virtual vector<Person> find_all_children_of(const string &name) = 0;
 };
 
@@ -33,13 +37,16 @@ struct Relationships : RelationshipBrowser
 
     void add_parent_and_child(const Person &parent, const Person &child)
     {
+
         relations.push_back({parent, Relationship::parent, child});
         relations.push_back({child, Relationship::child, parent});
     }
 
     vector<Person> find_all_children_of(const string &name) override
     {
+
         vector<Person> result;
+
         for (auto &&[first, rel, second] : relations)
         {
             if (first.name == name && rel == Relationship::parent)
@@ -57,24 +64,32 @@ struct Research
 
     // Research(const Relationships &relationships)
     // {
+
     //     auto &relations = relationships.relations;
 
     //     for (auto &&[first, rel, second] : relations)
     //     {
     //         if (first.name == "John" && rel == Relationship::parent)
     //         {
-    //             cout << "John has a child called " << second.name << endl;
+    //             cout << "John has a child called " << first.name << endl;
     //         }
     //     }
     // }
 
     Research(RelationshipBrowser &browser)
     {
+
         for (auto &child : browser.find_all_children_of("John"))
         {
             cout << "John has a child called " << child.name << endl;
         }
     }
 };
+
+/*Principle:
+1) High-level modules should not depend on low-level modules.
+Both should depend on abstractions/ virtual or base classes.
+2) Abstractions should not depend on details.
+Details should depend on abstractions.*/
 
 #endif
