c6db68b85c9f7bc3a97b4153b5b27fa8538f4e40
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-08 17:39:48 +0000

    training (again) with the Groovy Style Builder Pattern.

--- cpp/include/designPatterns_bits/groovyStyle.hpp
+++ cpp/include/designPatterns_bits/groovyStyle.hpp
@@ -19,6 +19,7 @@ namespace html
 
         friend ostream &operator<<(ostream &os, const Tag &tag)
         {
+
             os << "<" << tag.name;
 
             for (const auto &att : tag.attributes)
@@ -33,38 +34,50 @@ namespace html
 
             else
             {
+
                 os << ">" << endl;
                 if (tag.text.length())
                 {
                     os << tag.text << endl;
                 }
 
-                for (auto &child : tag.children)
+                for (const auto &child : tag.children)
                 {
                     os << child;
                 }
+
                 os << "</" << tag.name << ">" << endl;
             }
 
             return os;
         }
 
-    protected:
-        Tag(const string &name, const string &text) : name(name), text(text) {}
-        Tag(const string &name, const vector<Tag> children) : name(name), children(children) {}
+        Tag(const string &name, const string &text) : name(name), text(text)
+        {
+        }
+
+        Tag(const string &name, const vector<Tag> &children) : name(name), children(children)
+        {
+        }
     };
 
     struct P : Tag
     {
-        explicit P(const string &text) : Tag("p", text) {}
-        P(initializer_list<Tag> children) : Tag("p", children) {}
+        explicit P(const string &text) : Tag("p", text)
+        {
+        }
+
+        P(initializer_list<Tag> children) : Tag("p", children)
+        {
+        }
     };
 
     struct IMG : Tag
     {
-        explicit IMG(const string &url) : Tag("img", "")
+
+        explicit IMG(const string &url) : Tag("src", "")
         {
-            attributes.emplace_back(make_pair("src", url));
+            attributes.emplace_back(make_pair("img", url));
         }
     };
 
