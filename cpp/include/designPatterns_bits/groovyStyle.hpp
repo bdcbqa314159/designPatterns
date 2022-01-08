1c1044d071cbd2ed5962e39f62cf08fe6b0a764e
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-08 18:32:59 +0000

    training (again and again) with the Groovy Style Builder Pattern.

--- cpp/include/designPatterns_bits/groovyStyle.hpp
+++ cpp/include/designPatterns_bits/groovyStyle.hpp
@@ -7,13 +7,88 @@
 
 using namespace std;
 
+// namespace html
+// {
+
+//     struct Tag
+//     {
+
+//         string name, text;
+//         vector<Tag> children;
+//         vector<pair<string, string>> attributes;
+
+//         friend ostream &operator<<(ostream &os, const Tag &tag)
+//         {
+
+//             os << "<" << tag.name;
+
+//             for (const auto &att : tag.attributes)
+//             {
+//                 os << " " << att.first << "=\"" << att.second << "\"";
+//             }
+
+//             if (tag.children.size() == 0 && tag.attributes.size())
+//             {
+//                 os << "/>" << endl;
+//             }
+
+//             else
+//             {
+
+//                 os << ">" << endl;
+//                 if (tag.text.length())
+//                 {
+//                     os << tag.text << endl;
+//                 }
+
+//                 for (const auto &child : tag.children)
+//                 {
+//                     os << child;
+//                 }
+
+//                 os << "</" << tag.name << ">" << endl;
+//             }
+
+//             return os;
+//         }
+
+//         Tag(const string &name, const string &text) : name(name), text(text)
+//         {
+//         }
+
+//         Tag(const string &name, const vector<Tag> &children) : name(name), children(children)
+//         {
+//         }
+//     };
+
+//     struct P : Tag
+//     {
+//         explicit P(const string &text) : Tag("p", text)
+//         {
+//         }
+
+//         P(initializer_list<Tag> children) : Tag("p", children)
+//         {
+//         }
+//     };
+
+//     struct IMG : Tag
+//     {
+
+//         explicit IMG(const string &url) : Tag("src", "")
+//         {
+//             attributes.emplace_back(make_pair("img", url));
+//         }
+//     };
+
+// }
+
 namespace html
 {
 
     struct Tag
     {
-
-        string name, text;
+        string text, name;
         vector<Tag> children;
         vector<pair<string, string>> attributes;
 
@@ -27,15 +102,16 @@ namespace html
                 os << " " << att.first << "=\"" << att.second << "\"";
             }
 
-            if (tag.children.size() == 0 && tag.attributes.size())
+            if (tag.children.size() == 0 && tag.attributes.size() == 0)
             {
-                os << "/>" << endl;
+                os << "/>";
             }
 
             else
             {
 
                 os << ">" << endl;
+
                 if (tag.text.length())
                 {
                     os << tag.text << endl;
@@ -43,7 +119,7 @@ namespace html
 
                 for (const auto &child : tag.children)
                 {
-                    os << child;
+                    os << child << endl;
                 }
 
                 os << "</" << tag.name << ">" << endl;
@@ -56,18 +132,18 @@ namespace html
         {
         }
 
-        Tag(const string &name, const vector<Tag> &children) : name(name), children(children)
+        Tag(const string &name, const vector<Tag> children) : name(name), children(children)
         {
         }
     };
 
     struct P : Tag
     {
-        explicit P(const string &text) : Tag("p", text)
+        P(const string &text) : Tag("p", text)
         {
         }
 
-        P(initializer_list<Tag> children) : Tag("p", children)
+        explicit P(initializer_list<Tag> children) : Tag("p", children)
         {
         }
     };
