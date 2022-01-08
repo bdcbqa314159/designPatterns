9ed2b03e56a0d17f1d6762252eb7f04984ce0e6f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-08 13:22:30 +0000

    first part of Builder Pattern understood :).

--- cpp/include/designPatterns_bits/builderPattern.hpp
+++ cpp/include/designPatterns_bits/builderPattern.hpp
@@ -9,6 +9,105 @@
 
 using namespace std;
 
+// void badBuilder()
+// {
+//     string text = "hello";
+//     string out = "<p>" + text + "</p>";
+
+//     cout << out << endl;
+
+//     string words[] = {"hello", "world"};
+
+//     ostringstream oss;
+//     oss << "<ul>";
+
+//     for (auto &w : words)
+//     {
+//         oss << "<li>" << w << "</li>";
+//     }
+
+//     oss << "</ul>";
+
+//     cout << oss.str() << endl;
+// }
+
+// struct HtmlBuilder;
+
+// struct HtmlElement
+// {
+//     string name;
+//     string text;
+//     vector<HtmlElement> elements;
+
+//     const size_t indent_size = 2;
+
+//     HtmlElement() {}
+
+//     HtmlElement(const string &name, const string &text) : name(name), text(text) {}
+
+//     string str(int indent = 0) const
+//     {
+
+//         ostringstream oss;
+//         string i(indent_size * indent, ' ');
+//         oss << i << "<" << name << ">" << endl;
+
+//         if (text.size() > 0)
+//         {
+//             oss << string(indent_size * (indent + 1), ' ') << text << endl;
+//         }
+
+//         for (auto &element : elements)
+//         {
+//             oss << element.str(indent + 1) << endl;
+//         }
+
+//         oss << i << "</" << name << ">" << endl;
+//         return oss.str();
+//     }
+
+//     static unique_ptr<HtmlBuilder> build(string root_name)
+//     {
+//         return make_unique<HtmlBuilder>(root_name);
+//     }
+// };
+
+// struct HtmlBuilder
+// {
+//     HtmlElement root;
+
+//     HtmlBuilder(string root_name)
+//     {
+//         root.name = root_name;
+//     }
+
+//     HtmlBuilder &add_child(string name, string text)
+//     {
+
+//         HtmlElement element{name, text};
+//         root.elements.emplace_back(element);
+//         return *this;
+//     }
+
+//     HtmlBuilder *add_child2(string name, string text)
+//     {
+
+//         HtmlElement element{name, text};
+//         root.elements.emplace_back(element);
+//         return this;
+//     }
+
+//     string str()
+//     {
+//         return root.str();
+//     }
+
+//     operator HtmlElement() const
+//     {
+//         return root;
+//     }
+// };
+
 void badBuilder()
 {
 
@@ -17,16 +116,16 @@ void badBuilder()
 
     cout << out << endl;
 
-    string words[]{"hello", "world"};
-
+    string words[] = {"hello", "world"};
     ostringstream oss;
 
     oss << "<ul>";
 
     for (auto &w : words)
     {
-        oss << "<li>" << w << "</li>" << endl;
+        oss << "<li>" << w << "</li>";
     }
+
     oss << "</ul>";
 
     cout << oss.str() << endl;
@@ -37,8 +136,7 @@ struct HtmlBuilder;
 struct HtmlElement
 {
 
-    string name;
-    string text;
+    string name, text;
     vector<HtmlElement> elements;
 
     const size_t indent_size = 2;
@@ -51,25 +149,24 @@ struct HtmlElement
     {
     }
 
-    string str(int indent = 0)
+    string str(int indent = 0) const
     {
 
         ostringstream oss;
         string i(indent * indent_size, ' ');
         oss << i << "<" << name << ">" << endl;
 
-        if (text.size() > 0)
+        if (text.size())
         {
             oss << string(indent_size * (indent + 1), ' ') << text << endl;
         }
 
-        for (auto &w : elements)
+        for (auto &e : elements)
         {
-
-            oss << w.str(indent + 1);
+            oss << e.str(indent + 1) << endl;
         }
 
-        oss << i << "</" << name << ">" << endl;
+        oss << "</" << name << ">" << endl;
         return oss.str();
     }
 
@@ -91,25 +188,19 @@ struct HtmlBuilder
 
     HtmlBuilder &add_child(string name, string text)
     {
-
         HtmlElement e{name, text};
-
         root.elements.emplace_back(e);
-
         return *this;
     }
 
     HtmlBuilder *add_child2(string name, string text)
     {
-
         HtmlElement e{name, text};
-
         root.elements.emplace_back(e);
-
         return this;
     }
 
-    string str()
+    string str() const
     {
         return root.str();
     }
