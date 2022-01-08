d00df16ebc098336a53c4026a31e1c8536080a68
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-08 18:51:12 +0000

    training (again, again & again) with the Groovy Style Builder Pattern.

--- cpp/include/designPatterns_bits/groovyStyle.hpp
+++ cpp/include/designPatterns_bits/groovyStyle.hpp
@@ -7,82 +7,6 @@
 
 using namespace std;
 
-// namespace html
-// {
-
-//     struct Tag
-//     {
-
-//         string name, text;
-//         vector<Tag> children;
-//         vector<pair<string, string>> attributes;
-
-//         friend ostream &operator<<(ostream &os, const Tag &tag)
-//         {
-
-//             os << "<" << tag.name;
-
-//             for (const auto &att : tag.attributes)
-//             {
-//                 os << " " << att.first << "=\"" << att.second << "\"";
-//             }
-
-//             if (tag.children.size() == 0 && tag.attributes.size())
-//             {
-//                 os << "/>" << endl;
-//             }
-
-//             else
-//             {
-
-//                 os << ">" << endl;
-//                 if (tag.text.length())
-//                 {
-//                     os << tag.text << endl;
-//                 }
-
-//                 for (const auto &child : tag.children)
-//                 {
-//                     os << child;
-//                 }
-
-//                 os << "</" << tag.name << ">" << endl;
-//             }
-
-//             return os;
-//         }
-
-//         Tag(const string &name, const string &text) : name(name), text(text)
-//         {
-//         }
-
-//         Tag(const string &name, const vector<Tag> &children) : name(name), children(children)
-//         {
-//         }
-//     };
-
-//     struct P : Tag
-//     {
-//         explicit P(const string &text) : Tag("p", text)
-//         {
-//         }
-
-//         P(initializer_list<Tag> children) : Tag("p", children)
-//         {
-//         }
-//     };
-
-//     struct IMG : Tag
-//     {
-
-//         explicit IMG(const string &url) : Tag("src", "")
-//         {
-//             attributes.emplace_back(make_pair("img", url));
-//         }
-//     };
-
-// }
-
 namespace html
 {
 
@@ -156,7 +80,6 @@ namespace html
             attributes.emplace_back(make_pair("img", url));
         }
     };
-
 }
 
 #endif
